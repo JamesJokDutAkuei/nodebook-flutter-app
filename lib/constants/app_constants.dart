13abc273ae6fc323f3226fede75f27f2a2ba79b4
JamesJokDutAkuei <j.akuei@alutudent.com>
2025-07-05 18:04:49 +0200

    feat: add app constants for better code organization

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  static const String appName = 'Nodebook';
+  static const String appVersion = '1.0.0';
+  
+  // Password validation constants
+  static const int minPasswordLength = 8;
+  
+  // UI constants
+  static const double cardBorderRadius = 12.0;
+  static const double dialogBorderRadius = 16.0;
+  static const double buttonHeight = 50.0;
+}
